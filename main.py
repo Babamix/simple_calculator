e7c45b360fb9dcd9ffbfba537eabe4d8756e7052
Babamix <bagus.emeraldo@gmail.com>
2022-04-24 15:48:33 +0700

    menambahkan operasi baru

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ print("""Masukkan operasi :
         1: pemjumlahan
         2: penguarangan
         3: pembagian
-        4: perkalian""")
+        4: perkalian
+        5: pembagian modulus""")
 operasi = int(input("Masukkan operasi:"))
 
 # melakukan operasi terhadap angkanya
@@ -30,4 +31,5 @@ elif operasi == 3:
     print(angka1/angka2)
 elif operasi == 4:
     print(angka1*angka2)
-
+elif operasi == 5:
+    print(angka1%angka2)
