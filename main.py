ad4bb6eb6f4120bf26362cc0a810a8d8388607e2
Babamix <89849396+Babamix@users.noreply.github.com>
2022-04-24 15:43:58 +0700

    operasi modulus

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ print("""Masukkan operasi :
         1: pemjumlahan
         2: penguarangan
         3: pembagian
-        4: perkalian""")
+        4: perkalian
+        5: Modulus""")
 operasi = int(input("Masukkan operasi:"))
 
 # melakukan operasi terhadap angkanya
@@ -30,4 +31,6 @@ elif operasi == 3:
     print(angka1/angka2)
 elif operasi == 4:
     print(angka1*angka2)
+elif operasi == 5:
+    print(angka1%angka2)
 
