6188b4d1dbe8b053af0106c6713fc6b45b6dfa14
Babamix <bagus.emeraldo@gmail.com>
2022-04-24 14:26:03 +0700

    Menambahkan input pada main file

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# menerima input dari user:
+
+# angka1
+angka1 = int(input("Masukkan angka pertama:"))
+#angka2
+angka2 = int(input("Masukkan angka kedua:"))
+    
+# operasi yang dilakukan
+    
+    # 1 penjumlahan
+    # 2 penguarangan
+    # 3 pembagian
+    # 4 perkalian
+print("""Masukkan operasi :
+        1: pemjumlahan
+        2: penguarangan
+        3: pembagian
+        4: perkalian""")
+operasi = int(input("Masukkan operasi:"))
+# melakukan operasi terhadap angkanya
